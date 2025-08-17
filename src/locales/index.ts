1797e60ca1b021d72e47f046d0df1904d8c1a4b9
Alejandro Canek <alejandro.canek.dev@gmail.com>
2025-08-16 20:13:08 -0600

    feat: Add USA SVG flag and enhance language switcher component
    - Added USA flag SVG to the project.
    - Refactored LangSwitcher component to improve language detection and dropdown functionality.
    - Updated language data structure to include flag SVGs for each language.
    - Modified English and Spanish locale files to update hotel information and descriptions.
    - Improved styling for the language switcher and dropdown menu.
    - Adjusted static path generation for language routes.

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,5 +1,19 @@
-import es from "@/locales/es.json";
-import en from "@/locales/en.json";
+import esTranslations from './es.json';
+import enTranslations from './en.json';
 
-export const languages = { es, en };
-export const defaultLang = "es";
+export const LANGUAGES = {
+  es: {
+    name: 'Español',
+    flag: `<svg viewBox="0 0 15 10" width="15" height="10"><path d="M0 0h15v10H0z" fill="#c8102e"/><path d="M0 0h15v5H0z" fill="#ffc72c"/></svg>`,
+    translations: esTranslations
+  },
+  en: {
+    name: 'English',
+    flag: `<svg viewBox="0 0 5 3" width="15" height="10"><path fill="#012169" d="M0 0h5v3H0z"/><path fill="#FFF" d="m5 0v3h-5V0zM1.9 0v3H3V0zM0 1.1v.8h5v-.8z"/><path fill="#C8102E" d="M2.1 0v3h.8V0zM0 1.3v.4h5v-.4z"/></svg>`,
+    translations: enTranslations
+  }
+};
+
+export type AppLocale = keyof typeof LANGUAGES;
+
+export const defaultLang: AppLocale = 'es';
